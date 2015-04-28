4c77e875adcab394ebfcb47392605a2a8269f122
Travo100 <Travo100@gmail.com>
2015-04-27 22:34:26 -0700

    Added a timer button

--- app.js
+++ app.js
@@ -23,17 +23,25 @@
     updateUI(ct);
   });
 
-  for (var i = 1; i < imgArr.length+1; i++){
-    showThumbs(i);
-  }
-
+  
+  //Make thumbnails for imagres from main-gallery
   function showThumbs(i){
     $('.thumbs-gallery').append(
         $('<li><img src="img/img-'+i+'.jpg"></li>'));
   }
 
+  //Pass the parameter for the length of imgArr array
+  for (var i = 1; i < imgArr.length+1; i++){
+    showThumbs(i);
+  }
+
+  //cache the thumblist li
   var $thumbsList = $('.thumbs-gallery li');
+  
+  //add the class selected to the first li in the list
   $('.thumbs-gallery li:first-child').addClass('selected');
+  
+  //make an array of thumbs to use in updateUI
   var thumbsArray = $thumbsList.toArray();
 
   $thumbsList.on('click', function(e){
@@ -43,12 +51,22 @@
     $(this).addClass('selected').siblings().removeClass('selected');
   });
 
-
-  //$('.thumbs-gallery li').addClass('selected').siblings().removeClass('selected');
   function updateUI(ct) {
     $(imgArr[ct]).addClass('selected').siblings().removeClass('selected');
     $(thumbsArray[ct]).addClass('selected').siblings().removeClass('selected');
   }
 
+  function setTimer(time){
+    setInterval(function(){
+    ct++;
+    if (ct%imgArr.length == 0){
+      ct = 0;
+    }
+      updateUI(ct);
+    }, time)
+  }
+  $('#play-button').on('click',function(){
+    setTimer(1000);
+  });
 
 //});
